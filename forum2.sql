4eee5ac5edc22392ccc0cf2ea1d72e553fce838b
fanchDev <51495972+eIfo1@users.noreply.github.com>
2022-06-16 04:47:25 -0400

    sdg

---
+++ forum2.sql
@@ -0,0 +1,180 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 16, 2022 at 10:45 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `forum2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(25) NOT NULL,
+  `cat_desc` varchar(125) NOT NULL,
+  `cat_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `msg_id` int(11) NOT NULL,
+  `msg_sender` int(11) NOT NULL,
+  `msg_receiver` int(11) NOT NULL,
+  `msg_seen` tinyint(1) NOT NULL DEFAULT 0,
+  `msg_title` varchar(255) NOT NULL,
+  `msg_body` text NOT NULL,
+  `msg_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(11) NOT NULL,
+  `post_cat` int(11) NOT NULL,
+  `post_title` varchar(60) NOT NULL,
+  `post_body` text NOT NULL,
+  `post_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `post_locked` tinyint(1) NOT NULL DEFAULT 0,
+  `post_poster` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `replies`
+--
+
+CREATE TABLE `replies` (
+  `reply_id` int(11) NOT NULL,
+  `reply_post` int(11) NOT NULL,
+  `reply_poster` int(11) NOT NULL,
+  `reply_created` datetime NOT NULL,
+  `reply_body` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_password` text NOT NULL,
+  `user_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `user_status` varchar(250) NOT NULL DEFAULT 'Hi, I''m new here!',
+  `user_bio` text DEFAULT NULL,
+  `user_updated` datetime NOT NULL,
+  `user_email` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_password`, `user_created`, `user_status`, `user_bio`, `user_updated`, `user_email`) VALUES
+(2, 'asd', '$2y$10$aKeuQljYdJvhjE.lU1zkT./ieXLz2xmSVql.w3hsuGObFMP2PJ.Ri', '2022-06-14 21:47:00', 'woopdefucking doo', NULL, '0000-00-00 00:00:00', 'averyalexandercarroll08@gmail.com'),
+(3, 'asd2', '$2y$10$l2I6SYji2R/df5p77Fjpwe5.p1ZtMJle0NZiobz6PVAhs5/NH.DqG', '2022-06-14 21:52:07', 'Hi, I\'m new here!', NULL, '0000-00-00 00:00:00', 'averyalexandercarroll08@gmail.com'),
+(4, 'asd3', '$2y$10$Q/nzTcDIh/.NMgIhoaVkIuqqHAjWPr/j1dFtDOcy6aZWKA77LFzfW', '2022-06-14 21:52:58', 'Hi, I\'m new here!', NULL, '0000-00-00 00:00:00', 'averyalexandercarroll08@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`msg_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `replies`
+--
+ALTER TABLE `replies`
+  ADD PRIMARY KEY (`reply_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `replies`
+--
+ALTER TABLE `replies`
+  MODIFY `reply_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
