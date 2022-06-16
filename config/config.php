cf98c7cdd91106db0a2e8ba2a5770d60725100c7
AveryC <buttonmash61@gmail.com>
2022-06-16 12:00:21 -0400

    removed db vars from config and into env file

--- config/config.php
+++ config/config.php
@@ -1,11 +1,7 @@
 <?php
 date_default_timezone_set('America/New_York');
 
-$DATABASE_HOST = 'localhost';
-$DATABASE_USERNAME = 'root';
-$DATABASE_PASSWORD = 'DatabasePass';
-$DATABASE = 'forum2';
-
+require('env.php');
 try {
     $conn = OpenConnection($DATABASE_HOST, $DATABASE_USERNAME, $DATABASE_PASSWORD, $DATABASE);
 } catch (mysqli_sql_exception $e) {
