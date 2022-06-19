792c45151d76e97f84ee5f0e67a1d781cc0ba503
fanchDev <51495972+eIfo1@users.noreply.github.com>
2022-06-19 01:45:25 -0400

    fixed login

--- config/forms/login.php
+++ config/forms/login.php
@@ -2,35 +2,17 @@
 
 if (isset($_POST["submit"])) {
     $username = $_POST["username"];
-    $email = $_POST["email"];
     $password = $_POST["password"];
-    $passwordRepeat = $_POST["passwordRepeat"];
 
     require_once '../functions.php';
+    
     require_once '../config.php';
 
-    if (EmptyInputSignup($username, $email, $password, $passwordRepeat) !== false) {
-        header("location: ../../signup?error=Please fill in all fields!");
+    if (EmptyInputLogin($username, $password) !== false) {
+        header("location: ../../login?error=Please fill in all fields!");
         exit();
     }
-    if (InvalidUsername($username) !== false) {
-        header("location: ../../signup?error=Username contains forbidden characters.");
-        exit();
-    }
-    if (InvalidEmail($email) !== false) {
-        header("location: ../../signup?error=Invalid email. This usually means that you entered your email address incorrectly or your email provider is on our blocked list to prevent spam.");
-        exit();
-    }
-    if (InvalidPasswordMatch($password, $passwordRepeat) !== false) {
-        header("location: ../../signup?error=Your passwords do not match!");
-        exit();
-    }
-    if (UsernameExists($conn, $username) !== false) {
-        header("location: ../../signup?error=The username you entered has already been registered by another user. Try another variation of your desired username or pick a new one altogether.");
-        exit();
-    }
-
-    CreateUser($conn, $username, $email, $password);
+    LoginUser($conn, $username, $password);
 } else {
     header('location: ../../signup?error=Access Denied!');
 }
