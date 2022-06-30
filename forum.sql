1e5f835a175aedf90fd5979b73f4bba9e6ab6f10
fanchDev <51495972+eIfo1@users.noreply.github.com>
2022-06-30 09:39:38 -0400

    css fixes
    CSS fixes along with an updated SQL file with removed comments. CSS fixes include changing hard-coded colors to variables to enable theming. These fixes aren't finished yet, and will be finished tomorrow.

--- forum.sql
+++ forum.sql
@@ -1,31 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 16, 2022 at 10:45 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `forum2`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
 
 CREATE TABLE `categories` (
   `cat_id` int(11) NOT NULL,
@@ -33,12 +14,6 @@ CREATE TABLE `categories` (
   `cat_desc` varchar(125) NOT NULL,
   `cat_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
 
 CREATE TABLE `messages` (
   `msg_id` int(11) NOT NULL,
@@ -50,12 +25,6 @@ CREATE TABLE `messages` (
   `msg_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
 CREATE TABLE `posts` (
   `post_id` int(11) NOT NULL,
   `post_cat` int(11) NOT NULL,
@@ -66,12 +35,6 @@ CREATE TABLE `posts` (
   `post_poster` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `replies`
---
-
 CREATE TABLE `replies` (
   `reply_id` int(11) NOT NULL,
   `reply_post` int(11) NOT NULL,
@@ -79,12 +42,6 @@ CREATE TABLE `replies` (
   `reply_created` datetime NOT NULL,
   `reply_body` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
@@ -97,73 +54,37 @@ CREATE TABLE `users` (
   `user_email` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `categories`
---
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`cat_id`);
 
---
--- Indexes for table `messages`
---
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`msg_id`);
 
---
--- Indexes for table `posts`
---
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_id`);
 
---
--- Indexes for table `replies`
---
 ALTER TABLE `replies`
   ADD PRIMARY KEY (`reply_id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `categories`
---
 ALTER TABLE `categories`
   MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `messages`
---
 ALTER TABLE `messages`
   MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `posts`
---
 ALTER TABLE `posts`
   MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `replies`
---
 ALTER TABLE `replies`
   MODIFY `reply_id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
