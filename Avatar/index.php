369c896e1c4c1e8667a7be122d3ca8c3456c1180
fanchDev <51495972+eIfo1@users.noreply.github.com>
2022-07-16 12:25:03 -0400

    Cock and ba
    se

--- Avatar/index.php
+++ Avatar/index.php
@@ -1,18 +1,21 @@
-<?php 
+<?php
 // import the StackImage class
 require_once "../utils/StackImage.php";
 require_once "../config/functions.php";
 require_once "../config/config.php";
-$user_id = $_GET['id'];
-if(!CheckIfUserExists($user_id)) {
-  $Image = new StackImage($_SERVER["DOCUMENT_ROOT"]."/cdn/error.png");
-  $Image->Output();
+if (!isset($_GET['id'])) {
+  echo '<style>body {background-color: #1E1D1D; font-family: monospace; color: white;}</style>';
+  echo 'No ID provided.';
+} else {
+  if (!CheckIfUserExists($_GET['id'])) {
+    echo '<style>body {background-color: #1E1D1D; font-family: monospace; color: white;}</style>';
+    echo 'The provided ID does not match a user account.';
+  } else {
+    $Image = new StackImage($_SERVER["DOCUMENT_ROOT"] . "/cdn/Avatar.png");
+    $Image->AddLayer($_SERVER["DOCUMENT_ROOT"] . "/cdn/Avatar.png");
+    $Image->AddLayer($_SERVER["DOCUMENT_ROOT"] . "/cdn/Pants.png");
+    $Image->AddLayer($_SERVER["DOCUMENT_ROOT"] . "/cdn/Shirt.png");
+    $Image->CropImage();
+    $Image->Output();
 }
-
-$Image = new StackImage($_SERVER["DOCUMENT_ROOT"]."/cdn/Avatar.png");
-$Image->AddLayer($_SERVER["DOCUMENT_ROOT"]."/cdn/Avatar.png");
-$Image->AddLayer($_SERVER["DOCUMENT_ROOT"]."/cdn/Pants.png");
-$Image->AddLayer($_SERVER["DOCUMENT_ROOT"]."/cdn/Shirt.png");
-$Image->CropImage();
-$Image->Output();
-
+}
